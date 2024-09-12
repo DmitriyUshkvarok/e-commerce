9571255a1f789d0d3313b9764c5d42fe28807a59
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2024-09-12 23:59:21 +0300

    Added a comment "лл" to Container component.

--- src/Components/Container/Container.tsx
+++ src/Components/Container/Container.tsx
@@ -4,6 +4,7 @@ import { RootLayoutProps } from '@/src/app/layout';
 const Container = ({ children }: RootLayoutProps) => {
   return (
     <>
+      {/* лл */}
       <div className={styles.container}>{children}</div>
     </>
   );
