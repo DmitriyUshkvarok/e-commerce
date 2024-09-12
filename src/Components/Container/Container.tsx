ace2844e7ccca6c745426ffb31c41bb2f2563db4
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2024-09-12 23:23:15 +0300

    "Updated Node.js version in sonar.yml and refactored Container.tsx component"

--- src/Components/Container/Container.tsx
+++ src/Components/Container/Container.tsx
@@ -2,7 +2,11 @@ import styles from './_container.module.scss';
 import { RootLayoutProps } from '@/src/app/layout';
 
 const Container = ({ children }: RootLayoutProps) => {
-  return <div className={styles.container}>{children}</div>;
+  return (
+    <>
+      <div className={styles.container}>{children}</div>
+    </>
+  );
 };
 
 export default Container;
