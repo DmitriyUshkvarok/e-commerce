dea3c8ed6be329a121f974d8071a573d3ef0847e
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2024-09-13 00:07:43 +0300

    "Added an empty function and an empty div element to Container component"

--- src/Components/Container/Container.tsx
+++ src/Components/Container/Container.tsx
@@ -2,9 +2,14 @@ import styles from './_container.module.scss';
 import { RootLayoutProps } from '@/src/app/layout';
 
 const Container = ({ children }: RootLayoutProps) => {
+  // const beer = () => {
+  //   return 1;
+  // };
+
   return (
     <>
       {/* лл */}
+      <div></div>
       <div className={styles.container}>{children}</div>
     </>
   );
