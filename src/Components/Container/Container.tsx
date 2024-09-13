7c26a37a5e2bd4b248b3595c2e56609d3cc00ee3
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2024-09-13 14:34:26 +0300

    ...

--- src/Components/Container/Container.tsx
+++ src/Components/Container/Container.tsx
@@ -2,6 +2,10 @@ import styles from './_container.module.scss';
 import { RootLayoutProps } from '@/src/app/layout';
 
 const Container = ({ children }: RootLayoutProps) => {
+  if (1 + 1) {
+  }
+
+  const ren = 23;
   return <div className={styles.container}>{children}</div>;
 };
 
