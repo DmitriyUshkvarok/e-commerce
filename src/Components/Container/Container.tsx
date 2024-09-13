8bb43a5066ed0c1b42f4817866d676055db6336f
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2024-09-13 14:11:32 +0300

    Added a comment to Container.tsx.

--- src/Components/Container/Container.tsx
+++ src/Components/Container/Container.tsx
@@ -2,6 +2,7 @@ import styles from './_container.module.scss';
 import { RootLayoutProps } from '@/src/app/layout';
 
 const Container = ({ children }: RootLayoutProps) => {
+  // примеры для теста сонар с невалидым кодом и случаем когда все хорошо
   return <div className={styles.container}>{children}</div>;
 };
 
