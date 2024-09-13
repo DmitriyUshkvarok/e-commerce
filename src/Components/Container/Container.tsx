0fbd21fe0dafad733be32c6d4b308f8c81ce800a
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2024-09-13 13:57:50 +0300

    Added two new lines with a conditional statement and a variable declaration to the Container component.

--- src/Components/Container/Container.tsx
+++ src/Components/Container/Container.tsx
@@ -2,6 +2,10 @@ import styles from './_container.module.scss';
 import { RootLayoutProps } from '@/src/app/layout';
 
 const Container = ({ children }: RootLayoutProps) => {
+  if (1 - 1) {
+  }
+  const lrt = 1;
+
   return <div className={styles.container}>{children}</div>;
 };
 
