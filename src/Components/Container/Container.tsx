82c64a20a3ade5d68f9648f71bf3e87de282d431
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2024-09-13 14:45:49 +0300

    "Removed unnecessary code and fixed callback function in finally block"

--- src/Components/Container/Container.tsx
+++ src/Components/Container/Container.tsx
@@ -2,11 +2,7 @@ import styles from './_container.module.scss';
 import { RootLayoutProps } from '@/src/app/layout';
 
 const Container = ({ children }: RootLayoutProps) => {
-  // пример как работает сонар с ошибками 
-
-  if (1 + 1) { }
-  
-  let qwe = 34
+//  убираем ошибки для успешного деплоя
   return <div className={styles.container}>{children}</div>;
 };
 
