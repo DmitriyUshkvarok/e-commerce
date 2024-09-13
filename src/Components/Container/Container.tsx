e7ff81864c34b6276fa8369c869334a48525ada8
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2024-09-13 14:13:58 +0300

    "Added two lines of code with an if statement and a variable declaration to Container component"

--- src/Components/Container/Container.tsx
+++ src/Components/Container/Container.tsx
@@ -3,6 +3,10 @@ import { RootLayoutProps } from '@/src/app/layout';
 
 const Container = ({ children }: RootLayoutProps) => {
   // примеры для теста сонар с невалидым кодом и случаем когда все хорошо
+// не валидный код
+  if (1 + 1) {
+  }
+  const qwe = 1;
   return <div className={styles.container}>{children}</div>;
 };
 
