af87dfedd98cfd8656f866a7946d3dc884ee0916
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2024-09-13 13:59:48 +0300

    Remove unnecessary code in Container.tsx

--- src/Components/Container/Container.tsx
+++ src/Components/Container/Container.tsx
@@ -2,10 +2,6 @@ import styles from './_container.module.scss';
 import { RootLayoutProps } from '@/src/app/layout';
 
 const Container = ({ children }: RootLayoutProps) => {
-  if (1 - 1) {
-  }
-  const lrt = 1;
-
   return <div className={styles.container}>{children}</div>;
 };
 
