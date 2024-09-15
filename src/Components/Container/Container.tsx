88136a698775ac1dabd6cbf244e960c79151976e
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2024-09-15 14:24:03 +0300

    "Added API_KEY variable and conditional statement in Container component"

--- src/Components/Container/Container.tsx
+++ src/Components/Container/Container.tsx
@@ -2,7 +2,11 @@ import styles from './_container.module.scss';
 import { RootLayoutProps } from '@/src/app/layout';
 
 const Container = ({ children }: RootLayoutProps) => {
-//  убираем ошибки для успешного деплоя
+  //  убираем ошибки для успешного деплоя
+  const API_KEY = '1234567789qwe';
+  
+  if (API_KEY) {
+  }
   return <div className={styles.container}>{children}</div>;
 };
 
