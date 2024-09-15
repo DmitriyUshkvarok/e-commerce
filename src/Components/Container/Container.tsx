44262a425aae10cef17af7a3da05e38144c2c62d
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2024-09-15 19:40:06 +0300

    ,,,

--- src/Components/Container/Container.tsx
+++ src/Components/Container/Container.tsx
@@ -2,12 +2,6 @@ import styles from './_container.module.scss';
 import { RootLayoutProps } from '@/src/app/layout';
 
 const Container = ({ children }: RootLayoutProps) => {
-  const config = {
-    API_KEY: 'super-secret-key-12345',
-    API_LOCAL: 'djn77hhebdh',
-  };
-
-  fetch(`https://qwe?${config.API_KEY}`);
   return <div className={styles.container}>{children}</div>;
 };
 
